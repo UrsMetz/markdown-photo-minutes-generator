23f0bf47da2e3095ae0c606007d9e3fe8e09df4a
Urs Metz <urs.metz@gmx.de>
2023-05-18 21:35:16 +0200

    Improve error handling using anyhow

--- src/lib.rs
+++ src/lib.rs
@@ -8,30 +8,28 @@ struct Section {
     image_files: Vec<PathBuf>,
 }
 
+#[derive(Debug)]
 struct Minutes {
     sections: Vec<Section>,
 }
 
-fn create_minutes(path: &Path) -> Result<Minutes, String> {
-    let dir = fs::read_dir(path).expect("should be a ReadDir");
-    let names = dir
-        .map(|d| d.expect("should be a dir entry"))
-        .map(create_section);
+fn create_minutes(path: &Path) -> anyhow::Result<Minutes> {
+    let dir = fs::read_dir(path)?;
+    let names = dir.map(|e| create_section(e?));
     Ok(Minutes {
-        sections: names.collect::<Vec<_>>(),
+        sections: names.collect::<anyhow::Result<Vec<_>>>()?,
     })
 }
 
-fn create_section(dir_entry: DirEntry) -> Section {
-    let dir = fs::read_dir(dir_entry.path()).expect("should be a sub ReadDir");
+fn create_section(dir_entry: DirEntry) -> anyhow::Result<Section> {
+    let dir = fs::read_dir(dir_entry.path())?;
     let image_files = dir
-        .map(|d| d.expect("should be a dir entry"))
-        .map(|e| e.path())
-        .collect::<Vec<_>>();
-    Section {
+        .map(|e| Ok(e?.path()))
+        .collect::<anyhow::Result<Vec<_>>>()?;
+    Ok(Section {
         name: dir_entry.file_name().to_string_lossy().to_string(),
         image_files,
-    }
+    })
 }
 
 #[cfg(test)]
@@ -44,40 +42,51 @@ mod tests {
     use crate::{create_minutes, Section};
 
     #[test]
-    fn minutes_from_no_session_directory() {
-        let dir = tempfile::tempdir().expect("temp dir should be created");
+    fn minutes_from_non_existing_parent_dir_is_err() -> anyhow::Result<()> {
+        let dir = tempfile::tempdir()?;
 
-        let minutes = create_minutes(dir.path()).expect("minutes should be created");
-
-        assert_that!(minutes.sections).is_empty();
+        assert_that!(create_minutes(&dir.path().join("does_not_exist"))).is_err();
+        Ok(())
     }
 
     #[test]
-    fn minutes_from_single_session_directory_without_images() {
-        let dir = tempfile::tempdir().expect("temp dir should be created");
-        create_dir(dir.path().join("abc")).expect("sub dir should be created");
+    fn minutes_from_no_session_directory() -> anyhow::Result<()> {
+        let dir = tempfile::tempdir()?;
 
-        let minutes = create_minutes(dir.path()).expect("minutes should be created");
+        let minutes = create_minutes(dir.path())?;
+
+        assert_that!(minutes.sections).is_empty();
+        Ok(())
+    }
+
+    #[test]
+    fn minutes_from_single_session_directory_without_images() -> anyhow::Result<()> {
+        let dir = tempfile::tempdir()?;
+        create_dir(dir.path().join("abc"))?;
+
+        let minutes = create_minutes(dir.path())?;
 
         assert_that!(minutes.sections).contains_all_of(&vec![&Section {
             name: "abc".to_string(),
             image_files: vec![],
         }]);
+        Ok(())
     }
 
     #[test]
-    fn minutes_from_single_session_directory_with_images() {
-        let dir = tempfile::tempdir().expect("temp dir should be created");
+    fn minutes_from_single_session_directory_with_images() -> anyhow::Result<()> {
+        let dir = tempfile::tempdir()?;
         let section_path = dir.path().join("abc");
-        create_dir(&section_path).expect("sub dir should be created");
+        create_dir(&section_path)?;
         let image_path = section_path.join("abc.jpg");
-        fs::File::create(&image_path).expect("file should be created");
+        fs::File::create(&image_path)?;
 
-        let minutes = create_minutes(dir.path()).expect("minutes should be created");
+        let minutes = create_minutes(dir.path())?;
 
         assert_that!(minutes.sections).contains_all_of(&vec![&Section {
             name: "abc".to_string(),
             image_files: vec![image_path],
         }]);
+        Ok(())
     }
 }
