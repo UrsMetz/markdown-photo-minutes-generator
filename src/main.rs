806baaa1803c0d7bf52a149e9f57f0c373ff87e5
Urs Metz <urs.metz@gmx.de>
2024-04-07 17:21:22 +0200

    Add flag to omit image generation

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ struct ImageConversionOptions {
     input_root_path: PathBuf,
     output_root_path: PathBuf,
     online_base_path: String,
+    skip_image_conversion: bool,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -24,14 +25,24 @@ fn main() -> anyhow::Result<()> {
     let for_conversion =
         lib::create_minutes_for_conversion(minutes, options.output_root_path.as_path())?;
 
-    let vec = for_conversion.sections.clone();
-    vec.into_iter()
-        .flat_map(|s| s.image_files)
-        .try_for_each(|f| {
-            lib::image_operations::save_as_resized_image(&f.source_image_path, f.large_image, 1.0)?;
-            lib::image_operations::save_as_resized_image(&f.source_image_path, f.small_image, 0.4)?;
-            anyhow::Ok(())
-        })?;
+    if !options.skip_image_conversion {
+        let vec = for_conversion.sections.clone();
+        vec.into_iter()
+            .flat_map(|s| s.image_files)
+            .try_for_each(|f| {
+                lib::image_operations::save_as_resized_image(
+                    &f.source_image_path,
+                    f.large_image,
+                    1.0,
+                )?;
+                lib::image_operations::save_as_resized_image(
+                    &f.source_image_path,
+                    f.small_image,
+                    0.3,
+                )?;
+                anyhow::Ok(())
+            })?;
+    }
 
     let for_output = into_mintues_for_outputs(for_conversion, options.online_base_path)?;
 
@@ -69,11 +80,15 @@ fn options() -> OptionParser<ImageConversionOptions> {
     let input_root_path = bpaf::positional("INPUT");
     let output_root_path = bpaf::positional::<PathBuf>("OUTPUT");
     let online_base_path = bpaf::positional::<String>("ONLINE_BASE_PATH");
+    let skip_image_conversion = bpaf::long("skip-image-conversion")
+        .argument::<bool>("SKIP_IMAGE_CONVERSION")
+        .fallback(false);
 
     bpaf::construct!(ImageConversionOptions {
         input_root_path,
         output_root_path,
         online_base_path,
+        skip_image_conversion,
     })
     .to_options()
 }
